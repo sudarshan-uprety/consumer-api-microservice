38af75ed1057204d326b8300058883352f01c092
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-06-30 16:05:35 +0545

    fixed docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 
 echo "Starting authentication service"
-echo "Environment: $env"
-
-# Set the port based on the environment
-case "$env" in
-  "prod")
-    PORT=9000
-    ;;
-  "uat")
-    PORT=9002
-    ;;
-  "dev")
-    PORT=9001
-    ;;
-  *)
-    echo "Unknown environment: $env"
-    exit 1
-    ;;
-esac
 
 # Run Alembic migrations
 alembic upgrade head
-echo "Port is: $PORT"
 
 # Start the application with the appropriate port
-uvicorn main:app --host 0.0.0.0 --port $PORT
+uvicorn main:app --host 0.0.0.0 --port 5000
