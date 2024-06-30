098d3aef064a70fbd83b7823e2514c68b555f64d
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-06-30 15:39:56 +0545

    fixing docker issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -25,4 +25,4 @@ alembic upgrade head
 echo "Port is: $PORT"
 
 # Start the application with the appropriate port
-uvicorn main:app --host localhost --port $PORT
+uvicorn main:app --host 0.0.0.0 --port $PORT
