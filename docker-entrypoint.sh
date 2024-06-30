11d8808cf409a2620804fcdee319ff0a35009a90
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-06-30 12:22:16 +0545

    setup env for deveopment and github actions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,26 @@
-#!bin/bash
+#!/bin/bash
 
 echo "Starting authentication service"
 
+# Set the port based on the environment
+case "$ENV" in
+  "prod")
+    PORT=9002
+    ;;
+  "uat")
+    PORT=9001
+    ;;
+  "dev")
+    PORT=9000
+    ;;
+  *)
+    echo "Unknown environment: $ENV"
+    exit 1
+    ;;
+esac
+
+# Run Alembic migrations
 alembic upgrade head
 
-uvicorn main:app --host 0.0.0.0 --port 9000
+# Start the application with the appropriate port
+uvicorn main:app --host 0.0.0.0 --port $PORT
