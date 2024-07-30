3493d709e32feaf77b3442619108c97d3a7be168
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-07-30 14:32:55 +0545

    fixing

--- app/user/queries.py
+++ app/user/queries.py
@@ -40,6 +40,7 @@ def create_user(user):
     user_data['password'] = hashed_password
     user_data.pop('confirm_password', None)
     new_user = Users(**user_data)
+    new_user.is_active = True
     store.session.add(new_user)
     store.session.commit()
     return new_user
