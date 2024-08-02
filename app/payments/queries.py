c7e8622a8913744cc26efaad45dccc71cfd9c052
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-08-02 19:45:45 +0545

    API for payment and orders

--- app/payments/queries.py
+++ app/payments/queries.py
@@ -1,3 +1,4 @@
+from app.orders.models import Order
 from app.payments.models import UserPayment
 from app.payments.schema import TransactionDetails
 from app.user.models import Users
@@ -5,9 +6,11 @@ from utils import store
 
 
 def create_payment(payment: TransactionDetails, user: Users) -> UserPayment:
-    payment = payment.dict()
-    payment['user_id'] = user.id
-    payment_obj = UserPayment(**payment)
-    store.session.add(payment_obj)
+    data = payment.dict()
+    order_data = data.pop('order_details')
+    payment_data = data
+    payment_obj = UserPayment(**payment_data)
+    order_obj = Order(**order_data)
+    store.session.add(payment_obj, order_obj)
     store.session.commit()
     return payment_obj
