a49b50277ce9f00aa6816280033c447c3852750c
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-08-06 14:20:20 +0545

    create order and create payment completed

--- app/orders/models.py
+++ app/orders/models.py
@@ -20,7 +20,9 @@ class Orders(Common, Base, SerializerMixin):
     quantity = Column(Integer, nullable=False)
     price_per_item = Column(Float, nullable=False)
     payment_id = Column(Integer, ForeignKey('Payment.id'), nullable=False)
-
+    delivery_address = Column(String, nullable=False)
+    status = Column(String, nullable=True)
+    order_note = Column(String, nullable=True)
     user = relationship("Users", back_populates="orders")
 
     # payment = relationship("UserPayment", back_populates="orders")
--- app/orders/queries.py
+++ app/orders/queries.py
@@ -1,9 +1,26 @@
+from app.orders.models import Orders
 from app.orders.schemas import ProductOrder
 from app.payments.models import UserPayment
+from utils import store
 
 
 def create_order(orders: ProductOrder, payment: UserPayment):
-    print(orders)
-    for order in orders:
-        print('order is', order)
+    order_details = orders.dict()['order_details']
+    delivery_address = order_details['delivery_address']
+    order_notes = order_details['order_notes']
+    products = order_details['products']
+
+    for product in products:
+        order = Orders(
+            user_id=payment.user_id,
+            product_id=product['product_id'],
+            quantity=product['quantity'],
+            price_per_item=product['price_per_item'],
+            delivery_address=delivery_address,
+            payment_id=payment.id,
+            status=payment.payment_status,
+            order_note=order_notes
+        )
+        store.session.add(order)
+        store.session.commit()
     return None
